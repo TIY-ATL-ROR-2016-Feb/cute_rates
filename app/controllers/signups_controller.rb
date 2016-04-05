4e8d5782a2c4059302a1feb3553bf53323d5369a
Brit Butler <redline6561@gmail.com>
2016-04-05 10:35:12 -0400

    Add User token generation and basic signup method.

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,5 +1,14 @@
 class SignupsController < ApplicationController
   def create
-    render json: { message: "Good job. Stuff is happenin." }
+    @user = User.new(first_name: params["first"],
+                     last_name: params["last"],
+                     email: params["email"],
+                     password: params["password"])
+    @user.ensure_auth_token
+    if @user.save
+      render json: { user: @user.as_json(only: [:first_name, :last_name, :email, :auth_token ]) }
+    else
+      render json: { message: "That didn't work." }
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ActiveRecord::Base
     with: /.+\@.+\..+/, message: "must have an @ symbol and period"
   }
   validates :auth_token, presence: true
+
+  def ensure_auth_token
+    unless self.auth_token
+      self.auth_token = User.generate_token
+    end
+  end
+
+  def self.generate_token
+    token = SecureRandom.hex
+    while User.exists?(auth_token: token)
+      token = SecureRandom.hex
+    end
+    token
+  end
 end
