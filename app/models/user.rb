9d4c060a6db6c8b5694209f7cb520ad255125130
Brit Butler <redline6561@gmail.com>
2016-04-05 10:23:37 -0400

    Turn on has_secure_password and add some validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :email, presence: true, format: {
+    with: /.+\@.+\..+/, message: "must have an @ symbol and period"
+  }
+  validates :auth_token, presence: true
 end
