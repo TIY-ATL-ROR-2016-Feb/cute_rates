7b23915a6cb067faaaa0352007df15a92d2c19fe
Brit Butler <redline6561@gmail.com>
2016-04-06 09:50:21 -0400

    Add a post model, this one supports images.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405142042) do
+ActiveRecord::Schema.define(version: 20160406134600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "image_url"
+    t.string   "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
