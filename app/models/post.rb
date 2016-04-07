9eb054191aa644f8bbfb286bf25f939d780ae974
Brit Butler <redline6561@gmail.com>
2016-04-07 10:06:44 -0400

    Add paperclip methods to Post model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :ratings
 
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
   validates_presence_of :user_id
   validates :category, inclusion: {
               in: ["cats", "dogs", "small things", "old rats" ],
