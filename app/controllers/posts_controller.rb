cfb2d7aa93262efad3effa69ab1a238d2ee7046b
Brit Butler <redline6561@gmail.com>
2016-04-07 10:40:01 -0400

    Add route for creating posts and a jbuilder template!

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+  before_action :authenticate!, only: [:create]
+
+  def create
+    @post = current_user.posts.create(category: params["category"],
+                                      image: params["file"])
+    if @post.save
+      render "create.json.jbuilder", status: :created
+      # render json: {
+      #          post: {
+      #            user: @post.user,
+      #            category: @post.category,
+      #            image: @post.image.url,
+      #            created_at: @post.created_at,
+      #            updated_at: @post.updated_at
+      #          }
+      #        },
+      #        status: :created
+    else
+      render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def index
+
+  end
+end
---
+++ app/views/posts/create.json.jbuilder
@@ -0,0 +1,3 @@
+json.user @post.user, :email, :first_name, :last_name
+json.image_url @post.image.url
+json.category @post.category
