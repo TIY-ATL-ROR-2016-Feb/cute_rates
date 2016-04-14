73bd58ba02836f89da9091004a123dc51b9d5884
Brit Butler <redline6561@gmail.com>
2016-04-14 10:37:48 -0400

    Use rescue_from to avoid ever sending HTML for objects not in the database.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,9 @@ class ApplicationController < ActionController::Base
              status: :unauthorized
     end
   end
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { errors: e.message },
+           status: :not_found
+  end
 end
